baaa52aeda8390f1c5f6e16f1be967841429df81
Kito <kleeson@ualberta.ca>
2024-08-08 02:35:08 -0600

    new mongoose schemas for saving sessions to database

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 /** @format */
 
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const Session = require("./models/schema");
 require("dotenv").config();
+
+const app = express();
 
 // connect to mongodb
 const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@workout-tracker.bc8ncat.mongodb.net/`;
+mongoose
+	.connect(dbURI)
+	.then((result) => {
+		console.log("connected to db");
+		app.listen(3001, () => console.log("listening at 3001"));
+	})
+	.catch((error) => console.error(error));
 
-app.listen(3001, () => console.log("listening at 3001"));
 app.use(express.static("public")); // send client the to the "public" folder
 app.use(express.json()); // allow express to read incoming data as json
 
